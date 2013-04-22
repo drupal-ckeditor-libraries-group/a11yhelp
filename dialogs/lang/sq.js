ecd020ade2a536e2a5fcb1b16fd112afb7c9dc77
Wiktor Walc <w.walc@cksource.com>
2013-04-22 16:29:28 +0200

    Updated language files, added Albanian translation.

--- dialogs/lang/sq.js
+++ dialogs/lang/sq.js
@@ -3,30 +3,30 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.plugins.setLang( 'a11yhelp', 'uk', {
-	title: 'Спеціальні Інструкції',
-	contents: 'Довідка. Натисніть ESC і вона зникне.',
+CKEDITOR.plugins.setLang( 'a11yhelp', 'sq', {
+	title: 'Udhëzimet e Qasjes',
+	contents: 'Përmbajtja ndihmëse. Për ta mbyllur dialogun shtyp ESC.',
 	legend: [
 		{
-		name: 'Основне',
+		name: 'Të përgjithshme',
 		items: [
 			{
-			name: 'Панель Редактора',
+			name: 'Shiriti i Redaktuesit',
 			legend: 'Press ${toolbarFocus} to navigate to the toolbar. Move to the next and previous toolbar group with TAB and SHIFT-TAB. Move to the next and previous toolbar button with RIGHT ARROW or LEFT ARROW. Press SPACE or ENTER to activate the toolbar button.' // MISSING
 		},
 
 			{
-			name: 'Діалог Редактора',
+			name: 'Dialogu i Redaktuesit',
 			legend: 'Inside a dialog, press TAB to navigate to next dialog field, press SHIFT + TAB to move to previous field, press ENTER to submit dialog, press ESC to cancel dialog. For dialogs that have multiple tab pages, press ALT + F10 to navigate to tab-list. Then move to next tab with TAB OR RIGTH ARROW. Move to previous tab with SHIFT + TAB or LEFT ARROW. Press SPACE or ENTER to select the tab page.' // MISSING
 		},
 
 			{
-			name: 'Контекстне Меню Редактора',
+			name: 'Editor Context Menu', // MISSING
 			legend: 'Press ${contextMenu} or APPLICATION KEY to open context-menu. Then move to next menu option with TAB or DOWN ARROW. Move to previous option with SHIFT+TAB or UP ARROW. Press SPACE or ENTER to select the menu option. Open sub-menu of current option with SPACE or ENTER or RIGHT ARROW. Go back to parent menu item with ESC or LEFT ARROW. Close context menu with ESC.' // MISSING
 		},
 
 			{
-			name: 'Скринька Списків Редактора',
+			name: 'Editor List Box', // MISSING
 			legend: 'Inside a list-box, move to next list item with TAB OR DOWN ARROW. Move to previous list item with SHIFT + TAB or UP ARROW. Press SPACE or ENTER to select the list option. Press ESC to close the list-box.' // MISSING
 		},
 
@@ -37,35 +37,35 @@ CKEDITOR.plugins.setLang( 'a11yhelp', 'uk', {
 		]
 	},
 		{
-		name: 'Команди',
+		name: 'Komandat',
 		items: [
 			{
-			name: 'Відмінити команду',
-			legend: 'Press ${undo}' // MISSING
+			name: 'Rikthe komandën',
+			legend: 'Shtyp ${undo}'
 		},
 			{
-			name: ' Redo command', // MISSING
-			legend: 'Press ${redo}' // MISSING
+			name: 'Ribëj komandën',
+			legend: 'Shtyp ${redo}'
 		},
 			{
-			name: ' Bold command', // MISSING
-			legend: 'Press ${bold}' // MISSING
+			name: 'Komanda e trashjes së tekstit',
+			legend: 'Shtyp ${bold}'
 		},
 			{
-			name: ' Italic command', // MISSING
-			legend: 'Press ${italic}' // MISSING
+			name: 'Komanda kursive',
+			legend: 'Shtyp ${italic}'
 		},
 			{
-			name: ' Underline command', // MISSING
-			legend: 'Press ${underline}' // MISSING
+			name: 'Komanda e nënvijëzimit',
+			legend: 'Shtyp ${underline}'
 		},
 			{
-			name: ' Link command', // MISSING
-			legend: 'Press ${link}' // MISSING
+			name: 'Komanda e Nyjes',
+			legend: 'Shtyp ${link}'
 		},
 			{
 			name: ' Toolbar Collapse command', // MISSING
-			legend: 'Press ${toolbarCollapse}' // MISSING
+			legend: 'Shtyp ${toolbarCollapse}'
 		},
 			{
 			name: ' Access previous focus space command', // MISSING
@@ -76,8 +76,8 @@ CKEDITOR.plugins.setLang( 'a11yhelp', 'uk', {
 			legend: 'Press ${accessNextSpace} to access the closest unreachable focus space after the caret, for example: two adjacent HR elements. Repeat the key combination to reach distant focus spaces.' // MISSING
 		},
 			{
-			name: ' Accessibility Help', // MISSING
-			legend: 'Press ${a11yHelp}' // MISSING
+			name: 'Ndihmë Qasjeje',
+			legend: 'Shtyp ${a11yHelp}'
 		}
 		]
 	}
